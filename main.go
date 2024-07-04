5eed0c5946d76203f7748c40f4ccda8eba5e0723
Leila Massanova <49619073+FallBackITA27@users.noreply.github.com>
2024-07-04 21:38:57 +0200

    gitignore executable (#3)
    * gitignore exe
    
    * rename variables

--- main.go
+++ main.go
@@ -1,17 +1,17 @@
 package main
 
 import (
-  "net/http"
+	"net/http"
 
-  "github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-  r := gin.Default()
-  r.GET("/ping", func(c *gin.Context) {
-    c.JSON(http.StatusOK, gin.H{
-      "message": "pong",
-    })
-  })
-  r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	ginEngine := gin.Default()
+	ginEngine.GET("/ping", func(request *gin.Context) {
+		request.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	ginEngine.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
