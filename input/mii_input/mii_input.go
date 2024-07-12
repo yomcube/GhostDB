aee74e63bf2af3e858ae1415ebd8aa677aae67b6
FallBackITA27 <diegomassanova4@gmail.com>
2024-07-12 02:50:17 +0200

    bugfix

--- input/mii_input/mii_input.go
+++ input/mii_input/mii_input.go
@@ -12,59 +12,62 @@ import (
 	"golang.org/x/text/transform"
 )
 
-type Mii struct {
-	IsFemale             bool
-	BirthMonth           uint8
-	BirthDay             uint8
-	FavoriteColor        mii_fav_colors
-	IsFavorite           bool
-	MiiName              string // utf-16be encoded null-terminated-ish string with max 10 char.
-	Height               uint8
-	Width                uint8
-	MiiID                uint32
-	MiiCreationTimestamp time.Time // it is the rightmost 28 of the 32 bits in the mii id
-	ConsoleID            uint32    // the last three bytes are the last three bytes of the mac address, first byte is a checksum
-	FaceType             uint8
-	SkinTone             uint8
-	FaceFeatures         uint8
-	Mingling             bool
-	SourceType           mii_source_type
-	HairType             uint8
-	HairColor            uint8
-	HairFlip             bool
-	EyebrowType          uint8
-	EyebrowRotation      uint8
-	EyebrowColor         uint8
-	EyebrowSize          uint8
-	EyebrowVertical      uint8
-	EyebrowHorizontal    uint8
-	EyeType              uint8
-	EyeRotation          uint8
-	EyeVertical          uint8
-	EyeColor             uint8
-	EyeSize              uint8
-	EyeHorizontal        uint8
-	NoseType             uint8
-	NoseSize             uint8
-	NoseVertical         uint8
-	MouthType            uint8
-	MouthColor           uint8
-	MouthSize            uint8
-	MouthVertical        uint8
-	GlassesType          uint8
-	GlassesColor         uint8
-	GlassesSize          uint8
-	GlassesVertical      uint8
-	FacialHairMustache   uint8
-	FacialHairBeard      uint8
-	FacialHairColor      uint8
-	FacialHairSize       uint8
-	FacialHairVertical   uint8
-	MoleType             bool
-	MoleSize             uint8
-	MoleVertical         uint8
-	MoleHorizontal       uint8
-	CreatorName          string // utf-16be encoded null-terminated-ish string with max 10 char.
+// Kaitai Struct provided by HEYimHeroic: https://mii.tools/studio/ksy/mii_data_wii.ksy
+// This gets out all of the Wii data, and should resemble the SQL table
+// Skipped bits, or `unused_xx` according to the yaml file, are junk data.
+type Mii struct { // 						Index	Len	Info
+	IsFemale             bool            //	0x00.1	0.1
+	BirthMonth           uint8           //	0x00.2	0.4
+	BirthDay             uint8           //	0x00.6	0.5
+	FavoriteColor        mii_fav_colors  //	0x01.3	0.4
+	IsFavorite           bool            //	0x01.7	0.1
+	MiiName              string          //	0x02	20	utf-16be encoded null-terminated-ish string with max 10 char.
+	Height               uint8           //	0x16.1	0.7
+	Width                uint8           //	0x17.1	0.7
+	MiiID                uint32          //	0x18	4
+	MiiCreationTimestamp time.Time       //	0x18.4	3.4
+	ConsoleID            uint32          //	0x1C	4	the last three bytes are the last three bytes of the mac address, first byte is a checksum
+	FaceType             uint8           //	0x20	0.3
+	SkinTone             uint8           //	0x20.3	0.3	max = 5
+	FaceFeatures         uint8           //	0x20.6	0.4	max = 11
+	Mingling             bool            //	0x21.5	0.1
+	SourceType           mii_source_type //	0x21.6	0.2
+	HairType             uint8           //	0x22	0.7	max = 71
+	HairColor            uint8           //	0x22.7	0.3
+	HairFlip             bool            //	0x23.2	0.1
+	EyebrowType          uint8           //	0x24	0.5	max = 23
+	EyebrowRotation      uint8           //	0x24.5	0.5	max = 11
+	EyebrowColor         uint8           //	0x26	0.3
+	EyebrowSize          uint8           //	0x26.3	0.4 max = 8
+	EyebrowVertical      uint8           //	0x26.7	0.5	max = 18
+	EyebrowHorizontal    uint8           //	0x27.4	0.4	max = 12
+	EyeType              uint8           //	0x28	0.6	max = 47
+	EyeRotation          uint8           //	0x28.6	0.5 max = 7
+	EyeVertical          uint8           //	0x29.3	0.5	max = 18
+	EyeColor             uint8           //	0x2A	0.3	max = 5
+	EyeSize              uint8           //	0x2A.3	0.4 max = 7
+	EyeHorizontal        uint8           //	0x2A.7	0.4 max = 12
+	NoseType             uint8           //	0x2C	0.4	max = 11
+	NoseSize             uint8           //	0x2C.4	0.4	max = 8
+	NoseVertical         uint8           //	0x2D	0.5	max = 18
+	MouthType            uint8           //	0x2E	0.5	max = 23
+	MouthColor           uint8           //	0x2E.5	0.2	max = 2
+	MouthSize            uint8           //	0x2E.7	0.4	max = 8
+	MouthVertical        uint8           //	0x2F.3	0.5	max = 18
+	GlassesType          uint8           //	0x30	0.4	max = 8
+	GlassesColor         uint8           //	0x30.4	0.3	max = 5
+	GlassesSize          uint8           //	0x30.7	0.4	max = 7
+	GlassesVertical      uint8           //	0x31.3	0.5	max = 20
+	FacialHairMustache   uint8           //	0x32	0.2
+	FacialHairBeard      uint8           //	0x32.2	0.2
+	FacialHairColor      uint8           //	0x32.4	0.3
+	FacialHairSize       uint8           //	0x32.7	0.4	max = 8
+	FacialHairVertical   uint8           //	0x33.3	0.5	max = 16
+	MoleType             bool            //	0x34	0.1
+	MoleSize             uint8           //	0x34.1	0.4	max = 8
+	MoleVertical         uint8           //	0x34.5	0.5	max = 30
+	MoleHorizontal       uint8           //	0x35.2	0.5	max = 16
+	CreatorName          string          //	0x36	20	utf-16be encoded null-terminated-ish string with max 10 char.
 }
 
 func (mii *Mii) SetBirth(month byte, day byte) error {
@@ -366,155 +369,153 @@ func (mii *Mii) SetMoleHorizontal(moleHorizontal byte) error {
 
 func InitializeFromMiigx(inputBytes []byte) (Mii, error) {
 	outMii := Mii{}
-	if inputBytes[0]>>7 == 1 {
-		return outMii, errors.New("mii is invalid")
-	}
 
-	outMii.IsFemale = ((inputBytes[0] >> 6) & 0b00000001) > 0
+	outMii.IsFemale = ((inputBytes[0x00] >> 6) & 0b00000001) > 0
 
-	if err := outMii.SetBirth((inputBytes[0]<<2)&0b11110000, ((inputBytes[0]<<6)|(inputBytes[1]>>2))>>3); err != nil {
+	if err := outMii.SetBirth((inputBytes[0x00]>>2)&0b00001111, ((inputBytes[0x00]<<6)|(inputBytes[0x01]>>2))>>3); err != nil {
 		return outMii, err
 	}
 
-	if err := outMii.SetFavoriteColor((inputBytes[1] >> 1) & 0b00001111); err != nil {
+	if err := outMii.SetFavoriteColor((inputBytes[0x01] >> 1) & 0b00001111); err != nil {
 		return outMii, err
 	}
 
-	outMii.IsFavorite = (inputBytes[1] & 0b00000001) > 0
+	outMii.IsFavorite = (inputBytes[0x01] & 0b00000001) > 0
 
-	if err := outMii.SetNameFromUTF16BEBytes([20]byte(inputBytes[2:])); err != nil {
+	if err := outMii.SetNameFromUTF16BEBytes([20]byte(inputBytes[0x02:])); err != nil {
 		return outMii, err
 	}
 
-	outMii.SetHeight(inputBytes[22])
-	outMii.SetWidth(inputBytes[23])
-	outMii.MiiID = uint32(inputBytes[24])<<24 | uint32(inputBytes[25])<<16 | uint32(inputBytes[26])<<8 | uint32(inputBytes[27])
+	outMii.SetHeight(inputBytes[0x16])
+	outMii.SetWidth(inputBytes[0x17])
+	outMii.MiiID = uint32(inputBytes[0x18])<<24 | uint32(inputBytes[0x19])<<16 | uint32(inputBytes[0x1A])<<8 | uint32(inputBytes[0x1B])
 	outMii.MiiCreationTimestamp = time.Unix(1136073600+int64((outMii.MiiID<<4)>>2), 0)
-	outMii.ConsoleID = uint32(inputBytes[28])<<24 | uint32(inputBytes[29])<<16 | uint32(inputBytes[30])<<8 | uint32(inputBytes[31])
-	outMii.FaceType = inputBytes[32] >> 5
-	outMii.SkinTone = (inputBytes[32] >> 2) & 0b00000111
+	outMii.ConsoleID = uint32(inputBytes[0x1C])<<24 | uint32(inputBytes[0x1D])<<16 | uint32(inputBytes[0x1E])<<8 | uint32(inputBytes[0x1F])
+	outMii.FaceType = inputBytes[0x20] >> 5
+	outMii.SkinTone = (inputBytes[0x20] >> 2) & 0b00000111
 
-	if err := outMii.SetFaceFeatures(((inputBytes[32] << 2) | (inputBytes[33] >> 6)) & 0b00001111); err != nil {
+	if err := outMii.SetFaceFeatures(((inputBytes[0x20] << 2) | (inputBytes[0x21] >> 6)) & 0b00001111); err != nil {
 		return outMii, err
 	}
 
-	outMii.Mingling = (inputBytes[33] & 0b00000100) > 0
-	outMii.SourceType = mii_source_type(inputBytes[33] & 0b00000011)
+	outMii.Mingling = (inputBytes[0x21] & 0b00000100) > 0
+	outMii.SourceType = mii_source_type(inputBytes[0x21] & 0b00000011)
 
-	if err := outMii.SetHairType(inputBytes[34] >> 1); err != nil {
+	if err := outMii.SetHairType(inputBytes[0x22] >> 1); err != nil {
 		return outMii, err
 	}
 
-	outMii.HairColor = ((inputBytes[34] << 2) | (inputBytes[35] >> 6)) & 0b00000111
-	outMii.HairFlip = (inputBytes[35] & 0b00100000) > 0
+	outMii.HairColor = ((inputBytes[0x22] << 2) | (inputBytes[0x23] >> 6)) & 0b00000111
+	outMii.HairFlip = (inputBytes[0x23] & 0b00100000) > 0
 
-	if err := outMii.SetEyebrowType(inputBytes[36] >> 3); err != nil {
+	if err := outMii.SetEyebrowType(inputBytes[0x24] >> 3); err != nil {
 		return outMii, err
 	}
 
-	if err := outMii.SetEyebrowRotation(((inputBytes[36] << 1) | (inputBytes[37] >> 7)) & 0b00000111); err != nil {
+	if err := outMii.SetEyebrowRotation(((inputBytes[0x24] << 2) | (inputBytes[0x25] >> 6)) & 0b00001111); err != nil {
 		return outMii, err
 	}
 
-	outMii.EyebrowColor = inputBytes[38] >> 5
+	outMii.EyebrowColor = inputBytes[0x26] >> 5
 
-	if err := outMii.SetEyebrowSize((inputBytes[38] >> 1) & 0b00001111); err != nil {
+	if err := outMii.SetEyebrowSize((inputBytes[0x26] >> 1) & 0b00001111); err != nil {
 		return outMii, err
 	}
 
-	if err := outMii.SetEyebrowVertical(((inputBytes[38] << 4) | (inputBytes[39] >> 4)) & 0b00011111); err != nil {
+	if err := outMii.SetEyebrowVertical(((inputBytes[0x26] << 4) | (inputBytes[0x27] >> 4)) & 0b00011111); err != nil {
 		return outMii, err
 	}
 
-	if err := outMii.SetEyebrowHorizontal(inputBytes[39] & 0b00001111); err != nil {
+	if err := outMii.SetEyebrowHorizontal(inputBytes[0x27] & 0b00001111); err != nil {
 		return outMii, err
 	}
 
-	if err := outMii.SetEyeType(inputBytes[40] >> 2); err != nil {
+	if err := outMii.SetEyeType(inputBytes[0x28] >> 2); err != nil {
 		return outMii, err
 	}
 
-	outMii.EyeRotation = inputBytes[41] >> 5
+	outMii.EyeRotation = inputBytes[0x29] >> 5
 
-	if err := outMii.SetEyeVertical(inputBytes[41] & 0b00001111); err != nil {
+	if err := outMii.SetEyeVertical(inputBytes[0x29] & 0b00001111); err != nil {
 		return outMii, err
 	}
 
-	if err := outMii.SetEyeColor(inputBytes[42] >> 5); err != nil {
+	if err := outMii.SetEyeColor(inputBytes[0x2A] >> 5); err != nil {
 		return outMii, err
 	}
 
-	outMii.EyeSize = (inputBytes[42] >> 1) & 0b00000111
+	outMii.EyeSize = (inputBytes[0x2A] >> 1) & 0b00000111
 
-	if err := outMii.SetEyeHorizontal(((inputBytes[42] << 3) | (inputBytes[43] >> 5)) & 0b00001111); err != nil {
+	if err := outMii.SetEyeHorizontal(((inputBytes[0x2A] << 3) | (inputBytes[0x2B] >> 5)) & 0b00001111); err != nil {
 		return outMii, err
 	}
 
-	if err := outMii.SetNoseType(inputBytes[44] >> 4); err != nil {
+	if err := outMii.SetNoseType(inputBytes[0x2C] >> 4); err != nil {
 		return outMii, err
 	}
 
-	if err := outMii.SetNoseSize(inputBytes[44] & 0b00001111); err != nil {
+	if err := outMii.SetNoseSize(inputBytes[0x2C] & 0b00001111); err != nil {
 		return outMii, err
 	}
 
-	if err := outMii.SetNoseVertical(inputBytes[45] >> 3); err != nil {
+	if err := outMii.SetNoseVertical(inputBytes[0x2D] >> 3); err != nil {
 		return outMii, err
 	}
 
-	if err := outMii.SetMouthType(inputBytes[46] >> 3); err != nil {
+	if err := outMii.SetMouthType(inputBytes[0x2E] >> 3); err != nil {
 		return outMii, err
 	}
 
-	if err := outMii.SetMouthColor((inputBytes[46] >> 1) & 0b00000011); err != nil {
+	if err := outMii.SetMouthColor((inputBytes[0x2E] >> 1) & 0b00000011); err != nil {
 
 		return outMii, err
 	}
-	if err := outMii.SetMouthSize(((inputBytes[46] << 3) | (inputBytes[47] >> 3)) & 0b00001111); err != nil {
+
+	if err := outMii.SetMouthSize(((inputBytes[0x2E] << 3) | (inputBytes[0x2F] >> 5)) & 0b00001111); err != nil {
 		return outMii, err
 	}
 
-	if err := outMii.SetMouthVertical(inputBytes[47] & 0b00011111); err != nil {
+	if err := outMii.SetMouthVertical(inputBytes[0x2F] & 0b00011111); err != nil {
 		return outMii, err
 	}
 
-	if err := outMii.SetGlassesType(inputBytes[48] >> 4); err != nil {
+	if err := outMii.SetGlassesType(inputBytes[0x30] >> 4); err != nil {
 		return outMii, err
 	}
 
-	if err := outMii.SetGlassesColor((inputBytes[48] >> 1) % 0b00000111); err != nil {
+	if err := outMii.SetGlassesColor((inputBytes[0x30] >> 1) & 0b00000111); err != nil {
 		return outMii, err
 	}
 
-	outMii.GlassesSize = ((inputBytes[48] << 3) | (inputBytes[49] >> 5)) & 0b00001111
+	outMii.GlassesSize = ((inputBytes[0x30] << 3) | (inputBytes[0x31] >> 5)) & 0b00001111
 
-	if err := outMii.SetGlassesVertical(inputBytes[49] & 0b00001111); err != nil {
+	if err := outMii.SetGlassesVertical(inputBytes[0x31] & 0b00011111); err != nil {
 		return outMii, err
 	}
 
-	outMii.FacialHairMustache = inputBytes[50] >> 6
-	outMii.FacialHairBeard = (inputBytes[50] >> 4) & 0b00000011
-	outMii.FacialHairColor = (inputBytes[50] >> 1) & 0b00000111
+	outMii.FacialHairMustache = inputBytes[0x32] >> 6
+	outMii.FacialHairBeard = (inputBytes[0x32] >> 4) & 0b00000011
+	outMii.FacialHairColor = (inputBytes[0x32] >> 1) & 0b00000111
 
-	if err := outMii.SetFacialHairSize(((inputBytes[50] << 3) | (inputBytes[51] >> 5)) & 0b00001111); err != nil {
+	if err := outMii.SetFacialHairSize(((inputBytes[0x32] << 3) | (inputBytes[0x33] >> 5)) & 0b00001111); err != nil {
 		return outMii, err
 	}
 
-	if err := outMii.SetFacialHairVertical(inputBytes[51] & 0b00011111); err != nil {
+	if err := outMii.SetFacialHairVertical(inputBytes[0x33] & 0b00011111); err != nil {
 		return outMii, err
 	}
 
-	outMii.MoleType = (inputBytes[52] >> 7) > 0
+	outMii.MoleType = (inputBytes[0x34] >> 7) > 0
 
-	if err := outMii.SetMoleSize((inputBytes[52] >> 3) & 0b00001111); err != nil {
+	if err := outMii.SetMoleSize((inputBytes[0x34] >> 3) & 0b00001111); err != nil {
 		return outMii, err
 	}
 
-	if err := outMii.SetMoleVertical(((inputBytes[52] << 2) | (inputBytes[53] >> 6)) & 0b00011111); err != nil {
+	if err := outMii.SetMoleVertical(((inputBytes[0x34] << 2) | (inputBytes[0x35] >> 6)) & 0b00011111); err != nil {
 		return outMii, err
 	}
 
-	if err := outMii.SetMoleHorizontal((inputBytes[53] >> 1) & 0b00011111); err != nil {
+	if err := outMii.SetMoleHorizontal((inputBytes[0x35] >> 1) & 0b00011111); err != nil {
 		return outMii, err
 	}
 
